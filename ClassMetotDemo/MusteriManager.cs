f17d94ba36aa7a050e5e498c3284b6bca87a64bf
berk karakas <karakasberk@hotmail.com>
2021-01-19 20:59:27 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " sisteme eklendi");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " Sistemden silindi");
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Adi : " + musteri.Adi + ", " + "Musteri Soyadı : " + musteri.Soyadi + ", " + "Musteri Yasi : " + musteri.Yasi + ", " + "Musteri Memleketi : " + musteri.Memleketi + ", " + "Musteri Meslegi : " + musteri.Meslegi);
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Ahmet";
+            musteri1.Soyadi = "Yılmaz";
+            musteri1.Yasi = 30;
+            musteri1.Memleketi = "İstanbul";
+            musteri1.Meslegi = "Öğretmen";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Mehmet";
+            musteri2.Soyadi = "Çamlı";
+            musteri2.Yasi = 42;
+            musteri2.Memleketi = "İzmir";
+            musteri2.Meslegi = "Mühendis";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Selma";
+            musteri3.Soyadi = "Demirci";
+            musteri3.Yasi = 23;
+            musteri3.Memleketi = "Çanakkale";
+            musteri3.Meslegi = "Öğrenci";
+
+
+
+            MusteriManager musterimanager = new MusteriManager();
+            musterimanager.Ekle(musteri1);
+            musterimanager.Sil(musteri2);
+            musterimanager.Listele(musteri3);
+        }
+    }
+}
